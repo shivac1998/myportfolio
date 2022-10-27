f1122cf1c2705909ce26f542c627e55f42b09390
Shiva Chemburkar <samirshivachemburkar@gmail.com>
2022-10-27 11:53:44 -0700

    Added to wrong project

--- pages/video.js
+++
@@ -1,9 +0,0 @@
-import videoUrl from "../public/Greentree.mp4";
-
-const VideoItem = () => (
-  <video controls>
-    <source src={videoUrl} type="video/mp4" />
-  </video>
-);
-
-export default VideoItem;
